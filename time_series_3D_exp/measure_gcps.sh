22bf795a174555096ca55c8128c96432d19938bc
Frank Guldstrand <frank.guldstrand87@gmail.com>
2019-09-12 15:41:29 +0200

    Delete measure_gcps.sh

--- time_series_3D_exp/measure_gcps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export PATH=/home/buster/micmac7007/bin:$PATH # Location of MicMac
-
-cd tmp #Enter temperary working dir
-
-cp -R ../GCP.xml GCP.xml #Copy xml containing Ground Control Point (GCP) info from directory above.
-
-mm3d SaisieAppuisInitQT ".*.JPG" "Ori-G" GCP.xml Measure.xml #Start micmac GCP picking GUI.
